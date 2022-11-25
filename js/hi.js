42a5d7571292ea1233555d61ea40e9ff50bf1c5c
CLaireChoi1209 <119053002+CLaireChoi1209@users.noreply.github.com>
2022-11-25 21:57:44 +0900

    Update hi.js

--- js/hi.js
+++ js/hi.js
@@ -1,4 +1,4 @@
 console.log("환영합니다 이곳은 환상과 무지개의 행복하고 신선한 나라 슈슈슙샤샵샥스핀스시를먹는다 라는 나라입니다~")
-// for (var i = 1; i < 4; i++) {
-//     console.log("hi~")
-// }
+for (var i = 1; i < 4; i++) {
+     console.log("hi~")
+ }
